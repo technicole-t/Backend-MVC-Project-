b72b0329a577d8b6bc86e2640438578d7dfd66df
technicole-t <thorleynicole@gmail.com>
2020-03-12 12:27:09 +0000

    first test written for GET request to api/treasures

---
+++ spec/app.spec.js
@@ -0,0 +1,28 @@
+process.env.NODE_ENV = "test";
+const app = require("../app");
+const request = require("supertest");
+const { expect } = require("chai");
+const client = require(".././db/connection");
+
+after(() => {
+  client.destroy();
+});
+
+describe("/api/treasures", () => {
+  it("GET: returns status: 200 & all treasures including shop name and other details", () => {
+    return request(app)
+      .get("/api/treasures")
+      .expect(200)
+      .then(res => {
+        const treasure = res.body.treasures[0];
+        expect(treasure).to.have.all.keys(
+          "treasure_id",
+          "treasure_name",
+          "colour",
+          "age",
+          "cost_at_auction",
+          "shop_name"
+        );
+      });
+  });
+});
